01cd57d55a639be0a9ff68b0b9ef6fb0f45761b2
kaio <dev.kaiohenrique@gmail.com>
2024-03-13 15:02:16 -0300

    ADD database migrations

--- database/migrations/2024_03_13_170309_create_redirects_table.php
+++ database/migrations/2024_03_13_170309_create_redirects_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('redirects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('code');
+            $table->integer('status');
+            $table->string('url_destino');
+            $table->timestamp('last_access')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('redirects');
     }
 };
--- database/migrations/2024_03_13_171417_create_redirect_logs_table.php
+++ database/migrations/2024_03_13_171417_create_redirect_logs_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('redirect_logs', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('user_ip');
+            $table->string('user_agent');
+            $table->string('header_refer')->nullable();
+            $table->json('query_params')->nullable();
+            $table->timestamps();
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('redirect_logs');
     }
 };
